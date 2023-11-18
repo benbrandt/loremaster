5fb88d2e0482c4891d5194a8f8feb449ce7f54b8
Ben Brandt <benjamin.j.brandt@gmail.com>
2023-11-18 13:42:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# loremaster
+
+A collection of tools to help a Loremaster running a campaign with The One Ring 2E.
---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+/*!
+# loremaster
+
+A collection of tools to help a Loremaster running a campaign with The One Ring 2E.
+*/
+use spin_sdk::http::{IntoResponse, Request, Response};
+use spin_sdk::http_component;
+
+mod names;
+
+/// A simple Spin HTTP component.
+#[http_component]
+#[allow(clippy::needless_pass_by_value)]
+fn handle_loremaster(req: Request) -> impl IntoResponse {
+    println!("Handling request to {:?}", req.header("spin-full-url"));
+    Response::builder()
+        .status(200)
+        .header("content-type", "text/plain")
+        .body("Hello, Fermyon")
+        .build()
+}
