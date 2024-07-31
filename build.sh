91f7f13f6b02b60652d7eeaeed9ccdd80312361c
Ben Brandt <benjamin.j.brandt@gmail.com>
2024-07-31 08:08:06 +0200

    chore: update wac command for 0.5.0

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -x
 
 cargo component build --target wasm32-wasip1 --release --workspace
-wac encode -d loremaster:cultures=target/wasm32-wasip1/release/cultures.wasm -d loremaster:characters=target/wasm32-wasip1/release/characters.wasm -d loremaster:handler=target/wasm32-wasip1/release/loremaster.wasm ./wit/composition.wac -o target/wasm32-wasip1/release/handler.wasm
+wac compose -d loremaster:cultures=target/wasm32-wasip1/release/cultures.wasm -d loremaster:characters=target/wasm32-wasip1/release/characters.wasm -d loremaster:handler=target/wasm32-wasip1/release/loremaster.wasm ./wit/composition.wac -o target/wasm32-wasip1/release/handler.wasm
