c15aec2db64886b094e43df4c8aedd8d6dc3e4fc
Ben Brandt <benjamin.j.brandt@gmail.com>
2024-10-19 22:05:12 +0200

    chore: update to wasip2 targetr

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 
-cargo component build --target wasm32-wasip1 --release --workspace
-wac compose -d loremaster:cultures=target/wasm32-wasip1/release/cultures.wasm -d loremaster:characters=target/wasm32-wasip1/release/characters.wasm -d loremaster:handler=target/wasm32-wasip1/release/loremaster.wasm ./wit/composition.wac -o target/wasm32-wasip1/release/handler.wasm
+cargo component build --target wasm32-wasip2 --release --workspace
+wac compose -d loremaster:cultures=target/wasm32-wasip2/release/cultures.wasm -d loremaster:characters=target/wasm32-wasip2/release/characters.wasm -d loremaster:handler=target/wasm32-wasip2/release/loremaster.wasm ./wit/composition.wac -o target/wasm32-wasip2/release/handler.wasm
